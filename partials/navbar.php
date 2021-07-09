7f8c00d2fd96ae490c5dee6fd1787362f86e39b7
Aeol <carlj15.cj@gmail.com>
2021-07-09 17:05:18 +0800

    update navbar

--- partials/navbar.php
+++ partials/navbar.php
@@ -7,13 +7,17 @@
     <div id="ham-menu-cont" class="menu-cont">
         <div class="nav-menu-cont">
             <div class="close-cont">
-                <h2>MENU</h2>
+                <h2>rentALL</h2>
                 <h2 id="ham-menu-close-btn" class="fas fa-times"></h2>
             </div>
             <ul class="nav-menu-list">
-                <li><a href="/">HOME</a></li>
-                <li>CATALOGUE</li>
+                <li><a href="/pages/homepage">HOME</a></li>
+                <li><a href="/pages/sell-item">RENT ITEM</a></li>
+                <li><a href="/pages/profile">PROFILE</a></li>
+                <li><a href="/pages/catalogue">CATALOGUE</a></li>
                 <li>CATEGORIES</li>
+            </ul>
+            <ul class="nav-menu-list">
                 <li>ABOUT US</li>
                 <li>CONTACT US</li>
             </ul>
@@ -23,8 +27,8 @@
 
     <div class="desktop-nav-cont">
         <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="">Catalogue</a></li>
+            <li><a href="/pages/homepage">Home</a></li>
+            <li><a href="/pages/catalogue">Catalogue</a></li>
             <li><a href="">Categories</a></li>
             <li><a href="">About us</a></li>
             <li><a href="">Contact us</a></li>
@@ -32,11 +36,11 @@
     </div>
 
     <div class="right-nav-cont">
-        <span id="profile-nav">Profile</span>
+        <span id="profile-nav"><a href="/pages/profile">Profile</a></span>
         <div class="signupin-cont">
-            <span id="sign-up">Signup</span>
+            <span id="sign-up"><a href="/pages/signup">Sign up</a></span>
             <span>|</span>
-            <span>Login</span>
+            <span><a href="/pages/login">Login</a></span>
         </div>
     </div>
 </nav>
