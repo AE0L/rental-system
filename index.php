07f6c7326e2dacf317d8be8bf98d8513ad4c00e4
Aeol <carlj15.cj@gmail.com>
2021-07-10 13:18:32 +0800

    mysqli connection and login redirect

--- index.php
+++ index.php
@@ -1,40 +1,18 @@
 <?php
     session_start();
+
+    // MySQL connection
+    $mysqli = new mysqli('localhost', 'root', '', 'rental_system');
+
+    if ($mysqli -> connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+        exit();
+    }
+
+    // login redirect
+    if (!isset($_SESSION['logged_in'])) {
+        header('Location: /pages/login');
+    } else {
+        header('Location: /pages/homepage');
+    }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-        <title>Rental System</title>
-
-        <link rel="stylesheet" href="./css/global.css">
-        <link rel="stylesheet" href="./css/hamburger.css">
-    </head>
-
-    <body>
-
-        <?php
-            $title = 'Home';
-            include('./partials/navbar.php');
-        ?>
-        
-        <main>
-            <a href="./pages/item-view">item-view</a>
-            <a href="./pages/sell-item">sell-item</a>
-            <a href="./pages/homepage">sell-item</a>
-        </main>
-
-        <footer>
-            BSCS Project 2020
-        </footer>
-
-        <script src="./js/hamburger.js"></script>
-        <script src="https://kit.fontawesome.com/2b03626812.js" crossorigin="anonymous"></script>
-    </body>
-
-</html>
