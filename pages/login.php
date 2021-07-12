503fc0a6ae323be81457347ba825a26ae3f30eed
joseph-tupaen <mrhyde00017@gmail.com>
2021-07-12 22:49:34 +0800

    commite message

--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,6 +12,12 @@
         <link rel="stylesheet" href="../css/global.css">
         <link rel="stylesheet" href="../css/index-nav.css">
         <link rel="stylesheet" href="../css/login.css">
+
+        <script>
+            function toLogin() {
+                window.location.href="signup.php";
+            }
+        </script>
     </head>
 
     <body>
@@ -24,7 +31,9 @@
             </div>
         </header>
 
+
         <main>
+
             <div class="wrapper">
                 <div class="login-cont">
                     <span class="logo">rentALL</span>
@@ -36,11 +45,13 @@
                             <hr>
                             <span>or</span>
                         </div>
-                        <input id="signup" type="button" value="Sign up">
-                    </form>
+                        <input id="signup" type="button" value="Sign up" onclick="toLogin()" >
+                   </form>
                 </div>
             </div>
+
         </main>
+
 
         <script src="https://kit.fontawesome.com/2b03626812.js" crossorigin="anonymous"></script>
     </body>
--- pages/signup.php
+++ pages/signup.php
@@ -1,3 +1,4 @@
+<?php include '../php/signupserver.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,26 +19,30 @@
             include '../partials/index-nav.php';
         ?>
 
+
         <main>
             <div class="wrapper">
                 <div class="signup-cont">
                     <span class="signup-title">Sign up</span>
-                    <form action="" id="signup-form">
-                        <input type="text" name="first-name" id="first-name" placeholder="First Name">
-                        <input type="text" name="last-name" id="last-name" placeholder="Last Name">
-                        <input type="text" name="username" id="username" placeholder="Username (4-16 characters)">
-                        <input type="password" name="password" id="password" placeholder="Password">
-                        <input type="password" name="re-password" id="re-password" placeholder="Verify password">
-                        <input type="tel" name="mobile" id="mobile" placeholder="Mobile No.">
-                        <input type="text" name="address-1" id="address-1" placeholder="Address 1">
-                        <input type="text" name="address-2" id="address-2" placeholder="Address 2">
+                    <form method="POST" action="signup.php" id="signup-form">
+                        <?php include '../php/signuperrors.php'; ?>
 
-                        <input type="submit" id="submit" value="Create account">
+                        <input type="text" name="firstname" id="first-name" placeholder="First Name">
+                        <input type="text" name="lastname" id="last-name" placeholder="Last Name">
+                        <input type="text" name="email" id="email" placeholder="Email">
+                        <input type="text" name="username" id="username" placeholder="Username (4-16 characters)">
+                        <input type="password" name="password_1" id="password" placeholder="Password">
+                        <input type="password" name="password_2" id="re-password" placeholder="Verify password">
+                        <input type="text" name="mobile" id="mobile" placeholder="Mobile No.">
+                        <input type="text" name="address_1" id="address-1" placeholder="Address 1">
+                        <input type="text" name="address_2" id="address-2" placeholder="Address 2">
+
+                        <button type="submit" id="submit" name="reg_user">Create Account</button>
 
                         <div class="login-cont">
                             <div class="login-wrapper">
                                 <span>Already have an account?</span>
-                                <a href="">Log in</a>
+                                <a href="login.php">Log in</a>
                             </div>
                         </div>
                     </form>
